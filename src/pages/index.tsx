15e82128ad03bca2ec97958a627af9aab95aee31
Yass Taheri <94052180+yastudioo@users.noreply.github.com>
2022-06-05 22:20:07 +0430

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import {graphql} from 'gatsby'
+import Head from '../components/Head'
+import Layout from '../components/Layout'
+import Stars from '../components/Stars'
+import Home from '../components/Home'
+import {TitlePageQueryData} from '../types'
+
+interface IndexPageProps {
+  readonly data: TitlePageQueryData
+}
+
+const IndexPage: React.FC<IndexPageProps> = ({data}) => {
+  const siteTitle: string = data.site.siteMetadata.title
+
+  return (
+    <Layout title={siteTitle} transparentNavigation={true}>
+      <Stars />
+      <Head
+        title="Home"
+        image="/images/main_site.jpg"
+        keywords={[
+          `yastudio`,
+          `Yass Taheri`,
+          `Yass_taheri`,
+          `Yastudio`,
+          `یااستودیو`,
+          `یاس`,
+          `یاس طاهری`,
+          `طراحی وبسایت`,
+          `برنامه نویسی`,
+          `کرمان`
+        ]}
+      />
+      <section>
+        <Home />
+      </section>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      ...SiteTitle
+    }
+  }
+`
+
+export default IndexPage
