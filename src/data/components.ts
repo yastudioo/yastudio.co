b0b922ab0c966741a2be939f15d0ab1b17692e87
Yass Taheri <94052180+yastudioo@users.noreply.github.com>
2022-06-05 22:37:16 +0430

    Add files via upload

---
+++ src/data/components.ts
@@ -0,0 +1,14 @@
+export const components = {
+  navigation: {
+    helpMessage: {
+      mobile: `Swipe around the screen or zoom it`,
+      desktop: `Move around the screen or scroll it`
+    }
+  },
+  intro: {
+    title: `انسان غایت زیبایی هاست. \n  \"یا\" استودیو عمقِ زیبایی هات رو٬ پیدا می کنه.`
+  },
+  notFound: {
+    notFoundImageSrc: `../../../images/sup_seal.gif`
+  }
+}
