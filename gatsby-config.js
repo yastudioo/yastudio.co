fb570be1615fbdf88c5d6ca5b5d648df5519689f
Yass Taheri <94052180+yastudioo@users.noreply.github.com>
2022-06-05 21:47:38 +0430

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,152 @@
+'use strict'
+
+require('dotenv').config({
+  path: `.env`
+})
+
+module.exports = {
+  pathPrefix: '__GATSBY_IPFS_PATH_PREFIX__',
+  siteMetadata: {
+    title: `Yastudio.co`,
+    description: `Yastudio`,
+    siteUrl: `https://yastudio.co`,
+    image: `/images/yass_meta_image.jpg`,
+    googleSiteVerification:
+      process.env.GOOGLE_SITE_VERIFICATION ||
+      'G-NZTBC0L9L7',
+    author: {
+      name: `yastudio`,
+      intro: `انسان غایت زیبایی هاست . یا استادیو عمق زیباییهات رو پیدا می کنه.`,
+      url: `http://yastudio.co`,
+      email: `hey@yastudio.co`
+    },
+    social: [
+      {name: `Instagram`, socialUrl: `https://instagram.com/yastudio.official`},
+      {name: `Twitter`, socialUrl: `https://twitter.com/yasstaheri.nft`},
+      {name: `linkedin`, socialUrl: `https://linkedin.com/yastudio`}
+    ]
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+        name: `posts`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/projects`,
+        name: `projects`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/images`,
+        name: `images`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 600,
+              showCaptions: [`alt`],
+              markdownCaptions: true,
+              backgroundColor: 'none',
+              wrapperStyle: `border-bottom: none;`
+            }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1rem; max-width: 600px; height: 400px; margin: auto;`
+            }
+          },
+          {
+            resolve: 'gatsby-remark-video',
+            options: {
+              width: 600,
+              height: 'auto',
+              preload: 'auto',
+              muted: true,
+              autoplay: true,
+              playsinline: true,
+              controls: true,
+              loop: true
+            }
+          },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              offsetY: `100`,
+              // eslint-disable-next-line max-len
+              icon: `static/images/favicon/yass_fav.png`,
+              className: `autolink-headers`,
+              maintainCase: true,
+              removeAccents: true,
+              isIconAfterHeader: true
+            }
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: true,
+              showLineNumbers: false,
+              noInlineHighlight: false
+            }
+          },
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `http://yastudio.co`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `YassTaheri`,
+        short_name: `yastudio.co`,
+        start_url: `/`,
+        background_color: `#255799`,
+        theme_color: `#255799`,
+        display: `standalone`,
+        icon: `static/images/favicon/yass_fav.png`, // This path is relative to the root of the site.
+        crossOrigin: `use-credentials`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId:
+          process.env.GOOGLE_ANALYTICS_TRACKING_ID ||
+          'G-NZTBC0L9L7'
+      }
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-transformer-json`,
+    `gatsby-plugin-react-svg`,
+    `gatsby-plugin-meta-redirect`,
+    `gatsby-plugin-image`,
+    'gatsby-plugin-ipfs',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ]
+}
